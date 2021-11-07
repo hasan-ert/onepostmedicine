6536702ad413066fc7bbed05279e71f2ba3140bf
Ekin Savas <>
2021-11-07 21:27:28 +0300

    .

---
+++ frontend/src/components/Sub-Components/SideBar.js
@@ -0,0 +1,28 @@
+function PermanentDrawerLeft() {
+    return ( 
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar />
+      <List>
+        {['Home', 'Courses', 'Test and Quizes', 'User'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+    );
+  }
