a5787e9e2b43045a95377c4ab576d4df0da33030
Ekin Savas <>
2021-11-29 15:47:57 +0300

    sidebar push

--- frontend/src/components/Sub-Components/SideBar.js
+++ frontend/src/components/Sub-Components/SideBar.js
@@ -1,3 +1,14 @@
+import Drawer from '@mui/material/Drawer';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import Toolbar from '@mui/material/Toolbar';
+
+const drawerWidth = 240;
+
 function PermanentDrawerLeft() {
     return ( 
     <Drawer
