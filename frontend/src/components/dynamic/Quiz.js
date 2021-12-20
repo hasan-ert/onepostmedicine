35ecafd54cee953b9e91661ac6242cce12ef8bee
hasan-ert <68220593+hasan-ert@users.noreply.github.com>
2021-12-20 14:01:04 +0300

    fix bugs related to quiz page from lecture page & added Display users page in admin

--- frontend/src/components/dynamic/Quiz.js
+++ frontend/src/components/dynamic/Quiz.js
@@ -1,152 +1,133 @@
+import "../componentCss/MainComponents/Quiz.css";
 
-import"../componentCss/MainComponents/Quiz.css";
-
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-
+import { getUserData, getUserRef, updateUserData } from "../../api/user.API";
 import "../componentCss/site.css";
 import "../componentCss/MainComponents/Lecture.css";
 
-import {
-  collection,
-  getDoc,
-  getDocs,
-  query,
-  where,
-} from "firebase/firestore";
+import { collection, getDoc, getDocs, query, where } from "firebase/firestore";
 
 import { db } from "../../constants/firebase-config";
 import { UpEachWord } from "../../helpers/helpers";
 
-
 export default function Quiz() {
-	const question = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-	];
-
-     //States
-  const [questions, setQuestion] = useState({questions:
-	[
-
-		{
-			questionText: '',
-			answerOptions: [
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: true },
-				{ answerText: '', isCorrect: false },
-			],
-		},
-	  ]
-	});
-
-  
+  //States
+  const [userRef, setUserRef] = useState();
+  const [questions, setQuestion] = useState({
+    questions: [
+      {
+        questionText: "",
+        answerOptions: [
+          { answerText: "", isCorrect: false },
+          { answerText: "", isCorrect: false },
+          { answerText: "", isCorrect: true },
+          { answerText: "", isCorrect: false },
+        ],
+      },
+    ],
+  });
 
   //Params
-  const {coursename, name } = useParams();
-  
+  const { coursename, name } = useParams();
+
   //useEffects
   useEffect(() => {
     getidQuizData();
+    getUserData().then((res) => {
+      setUserRef(res);
+    });
   }, [coursename]);
-  console.log(coursename);
-
- 
-
 
   const getidQuizData = async () => {
-    const parent_course = coursename
-	console.log("function:",parent_course);
-    const q = query(collection(db, "quizzes"), where("parent_course", "==", parent_course));
+    const parent_course = coursename;
+    console.log("function:", parent_course);
+    const q = query(
+      collection(db, "quizzes"),
+      where("parent_course", "==", parent_course)
+    );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-        console.log("sorular",doc.data());
-        doc.data() !== undefined
-          ? setQuestion(doc.data())
-          : setQuestion({ questionText: "olmuyor" });
-      });
-};
+      console.log("sorular", doc.data());
+      doc.data() !== undefined
+        ? setQuestion(doc.data())
+        : setQuestion({ questionText: "olmuyor" });
+    });
+  };
 
-console.log("questions",questions.questions[0]);
+  console.log("questions", questions.questions[0]);
 
+  ////////////////////////////////
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
 
- 
-////////////////////////////////
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
+  useEffect(() => {
+    isPassed();
+  }, [showScore]);
 
-	const handleAnswerOptionClick = (isCorrect) => {
-		if (isCorrect) {
-			setScore(score + 1);
-		}
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
-	};
-	return (
-        <div className='wraper'> 
-            <div className='quizdiv'>
-			{showScore ? (
-				<div className='score-section'>
-					You scored {score} out of {questions.questions.length}
-				</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.questions.length}
-						</div>
-						<div className='question-text'>{questions.questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions.questions[currentQuestion].answerOptions.map((answerOption) => (
-							<button className="quizbutton"onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
-				</>
-			)}
-		</div>
-        </div>
-		
-	);
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const isPassed = () => {
+    debugger;
+    if (score > (2 * questions.questions.length) / 3) {
+      let temp = userRef;
+      temp.unfinished_courses = userRef.unfinished_courses;
+      temp.completed_courses = userRef.completed_courses;
+
+      let unfinished_index = temp.unfinished_courses.indexOf(coursename);
+      temp.unfinished_courses.splice(unfinished_index, 1);
+
+      temp.completed_courses = [...temp.completed_courses, coursename];
+      updateUserData(temp);
+    }
+  };
+  return (
+    <div className="wraper">
+      <div className="quizdiv">
+        {showScore ? (
+          <div className="score-section">
+            You scored {score} out of {questions.questions.length}
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {currentQuestion + 1}</span>/
+                {questions.questions.length}
+              </div>
+              <div className="question-text">
+                {questions.questions[currentQuestion].questionText}
+              </div>
+            </div>
+            <div className="answer-section">
+              {questions.questions[currentQuestion].answerOptions.map(
+                (answerOption) => (
+                  <button
+                    className="quizbutton"
+                    onClick={() =>
+                      handleAnswerOptionClick(answerOption.isCorrect)
+                    }
+                  >
+                    {answerOption.answerText}
+                  </button>
+                )
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
